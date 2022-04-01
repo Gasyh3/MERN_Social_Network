cc4065d2b623a1b1c731a4d2ffd19b4afdbbdd77
Gasyh3 <kevrakt@gmail.com>
2022-04-01 13:42:03 +0200

    Initialisation des config, db, server

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_LK}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to MONGODB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
